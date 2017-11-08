b2ae2204335dfe92e793b11fa6301510491ef73f
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-11-08 14:01:06 +0200

    Taso "valmis"

--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -10,6 +10,11 @@ public class Player : MonoBehaviour
 
 	public float moveSpeed = 10f;
 
+	bool hasMachete = false;
+	public SpriteRenderer machete;
+	public Sprite replaceSprite;
+	public Animator maattori;
+
 	void FixedUpdate ()
 	{
 		float movementx = Input.GetAxis ("Horizontal") * Time.deltaTime * moveSpeed;
@@ -17,15 +22,52 @@ public class Player : MonoBehaviour
 		rigb.AddForce (new Vector2 (movementx, movementy), ForceMode2D.Impulse);
 	}
 
-	void OnTriggerEnter2D (Collider2D other)
+	void OnTriggerStay2D (Collider2D other)
 	{
+		Vector2 newPos = transform.position;
+		Vector3 kameraPos = kamera.transform.position;
+
 		switch (other.tag) 
 		{
-		case "ToLeft":
-			//siirry vasemmalle
+		case "Left":
+			newPos = locations [0].position;
+			transform.position = newPos; 
+
+			kameraPos.x = locations [0].position.x;
+			kameraPos.y = locations [0].position.y;
+			kamera.transform.position = kameraPos;
 			break;
-		case "ToRight":
-			//siirry oikealle
+
+		case "Middle":
+			newPos = locations [1].position;
+			transform.position = newPos; 
+
+			kameraPos.x = locations [1].position.x;
+			kameraPos.y = locations [1].position.y;
+			kamera.transform.position = kameraPos;
+			break;
+
+		case "Right":
+			newPos = locations [2].position;
+			transform.position = newPos; 
+
+			kameraPos.x = locations [2].position.x;
+			kameraPos.y = locations [2].position.y;
+			kamera.transform.position = kameraPos;
+			break;
+		case "Vines":
+			if (Input.GetKeyDown (KeyCode.E) && hasMachete == true) 
+			{
+				maattori.SetBool ("Iscut", true);
+				other.GetComponent<BoxCollider2D> ().enabled = false;
+			}
+			break;
+		case "Mach":
+			if (Input.GetKeyDown (KeyCode.E)) 
+			{
+				hasMachete = true;
+				machete.sprite = replaceSprite;
+			}
 			break;
 		}
 	}
